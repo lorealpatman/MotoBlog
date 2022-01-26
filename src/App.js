39391127953c7e23ce74499dd2abeb1fe009d34b
loreal <loreal.patman@gmail.com>
2022-01-26 11:19:02 -0500

    finished all pages and components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Topbar from './components/Topbar';
-// import HomePage from './pages/HomePage';
-// import SinglePostPage from './pages/SinglePostPage';
-// import WritePage from './pages/WritePage';
+import HomePage from './pages/HomePage';
+import SinglePostPage from './pages/SinglePostPage';
+import WritePage from './pages/WritePage';
 import SettingsPage from './pages/SettingsPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 const App = () => {
+  const currentUser = true;
   return (
-    <>
+    <Router>
       <Topbar />
-      {/* <HomePage /> */}
-      {/* <SinglePostPage /> */}
-      {/* <WritePage /> */}
-      <SettingsPage />
-    </>
+      <Switch>
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
+        <Route path='/posts'>
+          <HomePage />
+        </Route>
+        <Route path='/register'>
+          {currentUser ? <HomePage /> : <RegisterPage />}
+        </Route>
+        <Route path='/login'>
+          {currentUser ? <HomePage /> : <LoginPage />}
+        </Route>
+        <Route path='/post/:id'>
+          <SinglePostPage />
+        </Route>
+        <Route path='/write'>
+          {currentUser ? <WritePage /> : <LoginPage />}
+        </Route>
+        <Route path='/settings'>
+          {currentUser ? <SettingsPage /> : <LoginPage />}
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,26 +1,53 @@
 import React from 'react';
 import me from '../images/motorcycle.jpg';
+import { Link } from 'react-router-dom';
 
 const TopBar = () => {
+  const user = true;
   return (
     <div className='top'>
       <div className='topLeft'>
         <i className='topIcon fab fa-facebook-square'></i>
-        <i className='topIcon fab fa-twitter-square'></i>
-        <i className='topIcon fab fa-pinterest-square'></i>
         <i className='topIcon fab fa-instagram-square'></i>
+        <i className='topIcon fab fa-pinterest-square'></i>
+        <i className='topIcon fab fa-twitter-square'></i>
       </div>
       <div className='topCenter'>
         <ul className='topList'>
-          <li className='topListItem'>HOME</li>
+          <li className='topListItem'>
+            <Link className='link' to='/'>
+              HOME
+            </Link>
+          </li>
           <li className='topListItem'>ABOUT</li>
           <li className='topListItem'>CONTACT</li>
-          <li className='topListItem'>WRITE</li>
-          <li className='topListItem'>LOGOUT</li>
+          <li className='topListItem'>
+            <Link className='link' to='/write'>
+              WRITE
+            </Link>
+          </li>
+          {user && <li className='topListItem'>LOGOUT</li>}
         </ul>
       </div>
       <div className='topRight'>
-        <img className='topImg' src={me} alt='' />
+        {user ? (
+          <Link className='link' to='/settings'>
+            <img className='topImg' src={me} alt='' />
+          </Link>
+        ) : (
+          <ul className='topList'>
+            <li className='topListItem'>
+              <Link className='link' to='/login'>
+                LOGIN
+              </Link>
+            </li>
+            <li className='topListItem'>
+              <Link className='link' to='/register'>
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
         <i className='topSearchIcon fas fa-search'></i>
       </div>
     </div>
